89b69efa17f04614fa00642cf1044d5e2030eb69
ianjoseph <ianjoseph593@gmail.com>
2023-02-22 03:32:43 -0800

    Service and service abstractions added.

---
+++ Service.Abstractions/IUserService.cs
@@ -0,0 +1,13 @@
+﻿
+using Contracts;
+
+namespace Services
+{
+    public interface IUserService
+    {
+        Task<UserInfoDto> CreateUser(UserInfoDto user);
+        Task DeleteUser(int id);
+        Task<IEnumerable<UserInfoDto>> GetAllUsers();
+        Task<UserInfoDto> GetSpecificUser(int id);
+    }
+}
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,37 +1,54 @@
 ﻿
 using Contracts;
+using Domain.Entities;
 using Domain.Repositories;
 using Mapster;
+using System.Security.Principal;
 
 namespace Services
 {
-    internal sealed class UserService
+    internal sealed class UserService : IUserService
     {
         private readonly IRepositoryManager _repositoryManager;
 
         public UserService(IRepositoryManager repositoryManager)
         {
-            _repositoryManager= repositoryManager;
+            _repositoryManager = repositoryManager;
         }
 
         public async Task<IEnumerable<UserInfoDto>> GetAllUsers()
         {
-            var users = _repositoryManager.UserCrudOperationsRepository.GetAllUsers();
+            var users = await _repositoryManager.UserCrudOperationsRepository.GetAllUsers();
             var userinfodto = users.Adapt<IEnumerable<UserInfoDto>>();
             return userinfodto;
         }
 
         public async Task<UserInfoDto> GetSpecificUser(int id)
         {
-            var specificUser = _repositoryManager.UserCrudOperationsRepository.GetUserById(id);
+            var specificUser = await _repositoryManager.UserCrudOperationsRepository.GetUserById(id);
             if (specificUser == null)
             {
                 throw new ArgumentNullException(nameof(specificUser));
             }
-            
+
             var specificUserDto = specificUser.Adapt<UserInfoDto>();
             return specificUserDto;
         }
-            
+
+        public async Task<UserInfoDto> CreateUser(UserInfoDto user)
+        {
+            var account = user.Adapt<UserModel>();
+
+            await _repositoryManager.UserCrudOperationsRepository.InsertUser(account);
+
+            return account.Adapt<UserInfoDto>();
+        }
+        public async Task DeleteUser(int id)
+        {
+            //var userToBeDeleted = user.Adapt<UserModel>();
+
+            await _repositoryManager.UserCrudOperationsRepository.DeleteUser(id);
+        }
+
     }
 }
