f0090124388a617ed470b5a0604bc3d65eaea7dc
ianjoseph <ianjoseph593@gmail.com>
2023-03-01 11:30:31 -0800

    Added the db and additional instructions.

--- Persistence/Repositories/RepositoryManager.cs
+++ Persistence/Repositories/RepositoryManager.cs
@@ -7,11 +7,10 @@ namespace Persistence.Repositories
     public class RepositoryManager : IRepositoryManager
     {
         private readonly Lazy<IUserCrudOperationsRepository>  _userCrudOperationsRepository;
-        //private readonly IConfiguration _configuration;
+       
 
         public RepositoryManager(IConfiguration configuration)
         {
-            //_configuration = configuration;
             _userCrudOperationsRepository = new Lazy<IUserCrudOperationsRepository>(() => new UserCrudOperationsRepository(configuration));
         }
         public IUserCrudOperationsRepository UserCrudOperationsRepository => _userCrudOperationsRepository.Value;
