219a47cfafffb721770b75a487dbe336121a7e90
ianjoseph <ianjoseph593@gmail.com>
2023-03-01 21:09:08 -0800

    Add db maybe

---
+++ UserDB/Script.PostDeployment1.sql
@@ -0,0 +1,19 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+if not exists (select 1 from dbo.[User])
+begin
+    insert into dbo.[User] (FirstName, LastName)
+    values ('Tim', 'Corey'),
+    ('Sue', 'Storm'),
+    ('John', 'Doe'),
+    ('Mary', 'Poppins');
+end
